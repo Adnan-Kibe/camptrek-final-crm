7714fe896f3fd242911c894ad0685c15835ec829
AdnanK1 <adnangitonga@gmail.com>
2025-09-22 01:06:02 +0300

    Added the title input

--- app/(dashboard)/itineraries/update/[id]/page.tsx
+++ app/(dashboard)/itineraries/update/[id]/page.tsx
@@ -1,10 +1,12 @@
 'use client'
 
+import CustomFormInput from '@/components/pages/itinerary/create/CustomFormInput'
 import { baseInstance } from '@/constants/api'
 import { ItineraryProps } from '@/constants/propConstants'
 import { useQuery } from '@tanstack/react-query'
 import { useParams } from 'next/navigation'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { FormProvider, useForm } from 'react-hook-form'
 
 const UpdatePage = () => {
   const params = useParams()
@@ -22,8 +24,25 @@ const UpdatePage = () => {
   })
 
   console.log(itinerary)
+
+  const methods = useForm()
+  const { handleSubmit, reset } = methods
+
+  useEffect(() => {
+    if(itinerary)
+      reset(itinerary)
+  }, [reset, itinerary])
+
+  const onSubmit = async (data: any) => {}
+
   return (
-    <div>page</div>
+    <div>
+      <FormProvider {...methods}>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <CustomFormInput title="Trip Title" name="title" placeholder="e.g., Amazing Safari" />
+          </form>
+      </FormProvider>
+    </div>
   )
 }
 
